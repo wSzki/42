70533ea3e28f81228ba19db7bde62c7c708372a3
wSzki <wsz@anche.no>
2021-03-25 20:40:26 +0100

    clean check walls

--- cub3d/srcs/parsers/map/check_walls.c
+++ cub3d/srcs/parsers/map/check_walls.c
@@ -6,89 +6,69 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 21:50:27 by wszurkow          #+#    #+#             */
-/*   Updated: 2021/03/25 14:12:50 by wszurkow         ###   ########.fr       */
+/*   Updated: 2021/03/25 20:39:34 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/cub3d.h"
 
-static int	check_nsew(char **map, int i, int j)
+
+static int check_neighbour(char **map, int x, int y, char c)
+{
+	if (c == 'N')
+		x--;
+	if (c == 'S')
+		x++;
+	if (c == 'W')
+		y--;
+	if (c == 'E')
+		y++;
+	if ((map[x][y] == ' ' || map[x][y] == '1'))
+		return (1);
+	return(0);
+}
+
+static int	check_nswe(char **map, int x, int y, int row_count)
 {
 	int flag;
 
-	flag = 0;
-	if (!((map[i - 1][j] == ' ' || map[i - 1][j] == '1') && \
-				(map[i + 1][j] == ' ' || map[i + 1][j] == '1') && \
-				(map[i][j - 1] == ' ' || map[i][j - 1] == '1') && \
-				(map[i][j + 1] == ' ' || map[i][j + 1] == '1')))
-		flag = 1;
+	flag = 1;
+	if (x == 0)
+		if (check_neighbour(map, x, y, 'S'))
+			if (check_neighbour(map, x, y, 'W'))
+				if (check_neighbour(map, x, y, 'E'))
+					flag = 0;
+	if (x == row_count)
+		if (check_neighbour(map, x, y, 'N'))
+			if (check_neighbour(map, x, y, 'W'))
+				if (check_neighbour(map, x, y, 'E'))
+					flag = 0;
+	if (x != 0 && x != row_count)
+		if (check_neighbour(map, x , y, 'N'))
+			if (check_neighbour(map, x, y, 'S'))
+				if (check_neighbour(map, x, y, 'W'))
+					if (check_neighbour(map, x, y, 'E'))
+						flag = 0;
 	return (flag);
 }
 
-static int	check_nsew_top_bottom(char **map, int i, int j, char c)
+void		check_walls(t_global *g, int row_count, int col_count)
 {
-	int flag;
-
-	flag = 0;
-	if (c == 'T')
-		if (!((map[i + 1][j] == ' ' || map[i + 1][j] == '1') && \
-					(map[i][j - 1] == ' ' || map[i][j - 1] == '1') && \
-					(map[i][j + 1] == ' ' || map[i][j + 1] == '1')))
-			flag = 1;
-	if (c == 'B')
-		if (!((map[i - 1][j] == ' ' || map[i - 1][j] == '1') && \
-					(map[i][j - 1] == ' ' || map[i][j - 1] == '1') && \
-					(map[i][j + 1] == ' ' || map[i][j + 1] == '1')))
-			flag = 1;
-	return (flag);
-}
-
-static int		check_first_and_last_line(t_global *g, int row, int column)
-{
-	int i;
-	int flag;
-	char	**map;
-
-	i = 0;
-	flag = 0;
-
-	map = g->map_data;
-	while (map[0][++i] && i < column - 1)
-	{
-		if (map[0][i] == ' ')
-			flag += check_nsew_top_bottom(map, 0, i, 'T');
-	}
-	i = 0;
-	while (map[row - 1][++i] && i < column - 1)
-	{
-
-		if (map[row - 1][i] == ' ')
-			flag += check_nsew_top_bottom(map, row - 1, i, 'B');
-	}
-	(void)row;
-	return (flag);
-}
-
-void		check_walls(t_global *g, int line_count, int largest_line)
-{
-	int		i;
-	int		j;
+	int		x;
+	int		y;
 	int		flag;
 	char	**map;
 
 	flag = 0;
 	map = g->map_data;
-	i = 0;
-	while (map[++i] && i < line_count - 1)
+	x = -1;
+	while (map[++x] && x < row_count)
 	{
-		j = 0;
-		while (map[i][++j] && j < largest_line - 1)
-		{
-			if (map[i][j] == ' ')
-				flag += check_nsew(map, i, j);
-		}
+		y = 0;
+		while (map[x][++y] && y < col_count - 1)
+			if (map[x][y] == ' ')
+				flag += check_nswe(map, x, y, row_count - 1);
 	}
-	flag += check_first_and_last_line(g, line_count, largest_line);
 	if (flag)
-		append_error(g, "", "Invalid map within borders\n");
+		append_error(g, "", "Invalid map - wall missing\n");
 }
