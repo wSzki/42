98a9a20be5aa3a1affd4d3a3ad9473531cf18311
wSzki <wsz@anche.no>
2021-06-08 20:35:04 +0200

    ok

--- push_swap/srcs/sorters/quarter_sort.c
+++ push_swap/srcs/sorters/quarter_sort.c
@@ -6,7 +6,7 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:29:00 by wszurkow          #+#    #+#             */
-/*   Updated: 2021/06/08 19:44:52 by wszurkow         ###   ########.fr       */
+/*   Updated: 2021/06/08 20:30:48 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,6 @@ void	quarter_sort(t_global *g, long smallest, long largest, int offset)
 	int	i;
 	int	direction;
 
-	i = -1;
-	while (++i < A_SIZE / 2)
-		if (A_DATA[i] > smallest && A_DATA[i] <= largest)
-			direction = 0;
-	direction = 0;
 	i = A_SIZE - offset;
 	while (A_SIZE - offset > (i * 3) / 4)
 	{
--- push_swap/srcs/sorters/sort.c
+++ push_swap/srcs/sorters/sort.c
@@ -6,7 +6,7 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 16:48:51 by wszurkow          #+#    #+#             */
-/*   Updated: 2021/06/08 19:40:16 by wszurkow         ###   ########.fr       */
+/*   Updated: 2021/06/08 20:25:50 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	normalise_list(t_global *g, int *offset)
 {
 	while (A_SIZE % 4 != 0)
 	{
-		scroll_to(g, A, find_smallest(A));
+		scroll_to(g, A, find_largest(A));
 		px(g, B_ID);
 		*offset += 1;
 	}
@@ -38,13 +38,10 @@ void	sort(t_global *g)
 	find_median(A, &median_a, find_smallest(A), find_largest(A));
 	find_median(A, &median_b, find_smallest(A), median_a);
 	find_median(A, &median_c, median_a, find_largest(A));
-	if (B_SIZE)
-		minimum = find_largest(B);
-	else
-		minimum = (long)find_smallest(A) - 1;
-	quarter_sort(g, minimum, median_b, 0);
-	quarter_sort(g, median_c, find_largest(A), offset);
+	quarter_sort(g, median_c, find_largest(A), 0);
 	quarter_sort(g, median_a, median_c, offset);
 	quarter_sort(g, median_b, median_a, offset);
+	minimum = (long)find_smallest(A) - 1;
+	quarter_sort(g, minimum, median_b, offset);
 	scroll_to(g, A, find_smallest(A));
 }
