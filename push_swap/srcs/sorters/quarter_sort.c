5d4b774e7301c9e8a9f8fa0f591b971c9b61e6d6
wSzki <wsz@anche.no>
2021-06-09 16:06:12 +0200

    norme

--- push_swap/srcs/sorters/quarter_sort.c
+++ push_swap/srcs/sorters/quarter_sort.c
@@ -6,7 +6,7 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 17:29:00 by wszurkow          #+#    #+#             */
-/*   Updated: 2021/06/09 16:02:12 by wszurkow         ###   ########.fr       */
+/*   Updated: 2021/06/09 16:05:54 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,16 @@ static int	find_closest_chunk(t_global *g, long smallest, long largest)
 	return (1);
 }
 
-int		quarter_sort(t_global *g, long smallest, long largest, long input)
+int	quarter_sort(t_global *g, long smallest, long largest, long input)
 {
 	int	i;
 	int	direction;
-	int ret;
-	int offset;
+	int	offset;
 
-	i = A_SIZE;
+	i = A_SIZE + 1;
 	offset = 0;
-	while ((i % 4) != 0)
-	{
+	while ((--i % 4) != 0)
 		offset++;
-		i--;
-	}
 	i = A_SIZE - offset;
 	while (A_SIZE - offset > (i * 3) / 4)
 	{
@@ -47,16 +43,14 @@ int		quarter_sort(t_global *g, long smallest, long largest, long input)
 		if (A_DATA[0] > smallest && A_DATA[0] <= largest)
 			px(g, B_ID);
 		else if (direction == 0)
-		{
 			rx(g, A_ID);
-		}
 		else if (direction == 1)
 			rrx(g, A_ID);
 	}
 	if (input != NO_VALUE)
 		scroll_to(g, A, input);
-	ret = find_smallest(B);
+	i = find_smallest(B);
 	while (B_SIZE > 0)
 		push_smallest_or_largest(g);
-	return (ret);
+	return (i);
 }
