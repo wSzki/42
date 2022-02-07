a1a3f2294925dbef09c8dd8e5989845dace8dc1d
wsz <59784520+wSzki@users.noreply.github.com>
2022-02-07 17:26:03 +0100

    kk

---
+++ wip/06/ex01/Data.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 17:02:10 by wszurkow          #+#    #+#             */
+/*   Updated: 2022/02/07 17:11:34 by wszurkow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __DATA_H__
+#define __DATA_H__
+
+#include <stdint.h>
+#include <cstdlib>
+#include <iostream>
+
+struct Data
+{
+	int value_a;
+	int value_b;
+	int value_c;
+	std::string str;
+};
+
+#endif
