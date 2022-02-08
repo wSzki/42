07cbb2798ee956dcd14c9696a11ecd87c4c85055
wsz <59784520+wSzki@users.noreply.github.com>
2022-02-08 01:50:25 +0100

    k

--- wip/07/ex01/main.cpp
+++ wip/07/ex01/main.cpp
@@ -6,25 +6,12 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 22:52:12 by wszurkow          #+#    #+#             */
-/*   Updated: 2022/02/07 23:15:22 by wszurkow         ###   ########.fr       */
+/*   Updated: 2022/02/08 01:50:06 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-
-	template <typename T>
-void t_print(T const & data)
-{
-	std::cout << data << std::endl;
-}
-
-	template <typename T>
-void iter(T *array, int array_len, void (*fun_ptr)(T const & param))
-{
-	for (int i = 0; i < array_len; i++)
-		(*fun_ptr)(array[i]);
-}
+#include "iterT.hpp"
 
 int main()
 {
@@ -32,14 +19,15 @@ int main()
 	char  array_char    [06] = {'a', 'b', 'c', 'd', 'e', 'f'};
 	float array_float   [02] = {-42.21, 21.42};
 	void *array_ptr     [03] = {array_int, array_char, array_float};
+
 	std::cout << "\n# INT" << std::endl;
-	iter(array_int,    10, t_print);
+	iter(array_int,    10, printT);
 	std::cout << "\n# CHAR" << std::endl;
-	iter(array_char,   06, t_print);
+	iter(array_char,   06, printT);
 	std::cout << "\n# FLOAT" << std::endl;
-	iter(array_float,  02, t_print);
+	iter(array_float,  02, printT);
 	std::cout << "\n# ADRESS" << std::endl;
-	iter(array_ptr,    03, t_print);
+	iter(array_ptr,    03, printT);
 	std::cout << std::endl;
 
 	return (0);
