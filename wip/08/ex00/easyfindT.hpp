1dffe86361ae5d2e042dbea2672b5ed3a1e591f9
wsz <59784520+wSzki@users.noreply.github.com>
2022-02-15 18:03:55 +0100

    ok

--- wip/08/ex00/easyfindT.hpp
+++ wip/08/ex00/easyfindT.hpp
@@ -6,19 +6,25 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:10:21 by wszurkow          #+#    #+#             */
-/*   Updated: 2022/02/10 21:14:12 by wszurkow         ###   ########.fr       */
+/*   Updated: 2022/02/15 18:02:34 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __EASYFIND_H__
-#define __EASYFIND_H__
-#include <iostream>
-#include <cctype>
+# define __EASYFIND_H__
+
+# include <iostream>
+# include <vector> // std::vector
+# include <algorithm> // std::find
+# include <exception>
 
 template <typename T>
 int easyfind(T t, int i)
 {
-
+	typename T::iterator iter = std::find(t.begin(), t.end(), i); // returns an iterator address
+	if (iter == t.end()) // t.end() points to the element after last element in container
+		throw std::exception();
+	return (*iter);
 }
 
 #endif
--- wip/08/ex00/main.cpp
+++ wip/08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:10:00 by wszurkow          #+#    #+#             */
-/*   Updated: 2022/02/10 21:13:02 by wszurkow         ###   ########.fr       */
+/*   Updated: 2022/02/15 18:02:02 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,55 @@ int main(int ac, char **av, char **env)
 	(void)av;
 	(void)env;
 
+	//std::vector<int>  array_i = {0, 9, 5, 6}; // cannot init like this (C++98)
+	//std::vector<char> array_c = {'a', 'b', 'c'};
 
-	return (0);
+	std::vector<int>  array_i;
+
+	array_i.push_back(0);
+	array_i.push_back(9);
+	array_i.push_back(5);
+	array_i.push_back(6);
+
+	std::vector<char>  array_c;
+
+	array_c.push_back('a');
+	array_c.push_back('b');
+	array_c.push_back('c');
+
+	std::cout << std::endl;
+	std::cout << "Iterating thru a container of ints" << std::endl;
+	for (std::vector<int>::iterator i = array_i.begin(); i != array_i.end(); i++)
+		std::cout << *i << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Iterating thru a container of ints" << std::endl;
+	for (std::vector<char>::iterator i = array_c.begin(); i != array_c.end(); i++)
+		std::cout << *i << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Searching 0, 9, 5, 6, then 7 which does not exist in the container" << std::endl;
+	try
+	{
+		std::cout << easyfind(array_i, 0) << " is the found number" <<  std::endl;
+		std::cout << easyfind(array_i, 9) << " is the found number" <<  std::endl;
+		std::cout << easyfind(array_i, 5) << " is the found number" <<  std::endl;
+		std::cout << easyfind(array_i, 6) << " is the found number" <<  std::endl;
+		std::cout << easyfind(array_i, 7) << " is the found number" <<  std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Number not found" << std::endl;
+	}
+	std::cout << std::endl;
+	std::cout << "Searching Invalid number" << std::endl;
+	try
+	{
+		std::cout << easyfind(array_i, -2345) << " is the found number" <<  std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Number not found" << std::endl;
+	}
+	std::cout << std::endl;
 }
